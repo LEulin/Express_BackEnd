c0fe41ccd92d83811dcbe6601b3e7c4682950e61
XhaviaKind <07judilynb@gmail.com>
2019-12-07 11:23:27 +0800

    updated account

--- model/User.js
+++ model/User.js
@@ -26,4 +26,12 @@ const userSchema = new Schema({
     }
 });
 
+userSchema.statics.updateUser = async function(username, newname, newPassword) {
+    return await this.updateOne({"username" : username}, {$set : {"name" : newname, "password" : newPassword}})
+}
+
+userSchema.statics.getUserByUsername = async function(username) {
+    return await this.findOne({"username" : username})
+}
+
 module.exports = mongoose.model('users', userSchema);
